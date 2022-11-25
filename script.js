5ceb2a7d19509247b2ca4a221f313a1a84816f53
Nayquo <greg.2503@live.fr>
2022-11-26 00:59:09 +0100

    add button with eventlistener for display dice

--- script.js
+++ script.js
@@ -5,41 +5,53 @@ const circleFour = document.querySelector(".four")
 const circleFive = document.querySelector(".five")
 const circleSix = document.querySelector(".six")
 const circleSeven = document.querySelector(".seven")
+const rollDice = document.querySelector(".rollDice")
 
-const rollDice = randomNumber(1, 6)
+refresh()
+
+function refresh() {
+    circleOne.style.display = "none"
+    circleTwo.style.display = "none"
+    circleThree.style.display = "none"
+    circleFour.style.display = "none"
+    circleFive.style.display = "none"
+    circleSix.style.display = "none"
+    circleSeven.style.display = "none"
+}
 
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function displayDiceCircle() {
-    rollDice
-    if (rollDice == 1) {
+    refresh()
+    const diceNumber = randomNumber(1, 6)
+    if (diceNumber == 1) {
         console.log("1")
         circleFour.style.display = "block"
-    } else if (rollDice == 2) {
+    } else if (diceNumber == 2) {
         console.log("2")
         circleThree.style.display = "block"
         circleFive.style.display = "block"
-    } else if (rollDice == 3) {
+    } else if (diceNumber == 3) {
         console.log("3")
         circleThree.style.display = "block"
         circleFour.style.display = "block"
         circleFive.style.display = "block"
-    } else if (rollDice == 4) {
+    } else if (diceNumber == 4) {
         console.log("4")
         circleOne.style.display = "block"
         circleThree.style.display = "block"
         circleFive.style.display = "block"
         circleSeven.style.display = "block"
-    } else if (rollDice == 5) {
+    } else if (diceNumber == 5) {
         console.log("5")
         circleOne.style.display = "block"
         circleThree.style.display = "block"
         circleFour.style.display = "block"
         circleFive.style.display = "block"
         circleSeven.style.display = "block"
-    } else if (rollDice == 6) {
+    } else if (diceNumber == 6) {
         console.log("6")
         circleOne.style.display = "block"
         circleTwo.style.display = "block"
@@ -50,4 +62,4 @@ function displayDiceCircle() {
     }
 }
 
-displayDiceCircle()
+addEventListener("click", displayDiceCircle) 
